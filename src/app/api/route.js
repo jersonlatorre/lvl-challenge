b995a02a6c0cf573d5dcccd0d8c7bcecee99918b
Jerson La Torre <jerson.latorre@gmail.com>
2024-04-29 09:25:34 -0500

    test: dall-e3 por dall-e2

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -23,7 +23,7 @@ const requestImage = async (phrase) => {
   const prompt = `creame una pintura impresionista, con los elementos bien reconocibles, sin texto, que se parezca a la siguiente oración: "${phrase}"`
 
   const response = await openai.images.generate({
-    model: 'dall-e-2',
+    model: 'dall-e-3',
     prompt: prompt,
     n: 1,
     size: '1024x1024',
