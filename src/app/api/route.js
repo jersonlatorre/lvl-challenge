c4c6d0d509539aa8474e28f9edbb9a3ac7cdc328
Jerson La Torre <jerson.latorre@gmail.com>
2024-04-30 12:04:20 -0500

    cambio a dalle-3

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -29,10 +29,10 @@ const requestImage = async (phrase) => {
   const prompt = `creame una pintura impresionista, con los elementos bien reconocibles, sin texto, que se parezca a la siguiente oración: "${phrase}"`
 
   const response = await openai.images.generate({
-    model: 'dall-e-2',
+    model: 'dall-e-3',
     prompt: prompt,
     n: 1,
-    size: '256x256',
+    size: '1024x1024',
   })
   
   return response.data[0].url
